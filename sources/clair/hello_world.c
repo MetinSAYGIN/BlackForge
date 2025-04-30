85bf048b9e9fbb3c221070fe9d0a4123c8c57f6e
SAYGIN Metin <56536431+MetinSAYGIN@users.noreply.github.com>
2025-04-30 16:32:39 +0200

    Update hello_world.c

--- sources/clair/hello_world.c
+++ sources/clair/hello_world.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-  printf("Bonjour");
-  return 0;
+int somme(int a, int b) {
+    return a + b;
+}
+
+int produit(int a, int b) {
+    return a * b;
+}
+
+int main() {
+    int x = 5, y = 3;
+    printf("Somme : %d\n", somme(x, y));
+    printf("Produit : %d\n", produit(x, y));
+    return 0;
 }
