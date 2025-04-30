50fb741d718077dcba62fff82bb2747949d0d9f8
SAYGIN Metin <56536431+MetinSAYGIN@users.noreply.github.com>
2025-04-30 17:37:02 +0200

    Update DeadBlockInsertion.cpp

--- passes/DeadBlockInsertion.cpp
+++ passes/DeadBlockInsertion.cpp
@@ -21,10 +21,13 @@ struct DeadBlockInsertionPass : public PassInfoMixin<DeadBlockInsertionPass> {
         Module *M = F.getParent();
         LLVMContext &Ctx = F.getContext();
 
-        // 1. Préparer les éléments essentiels
-        FunctionCallee PrintfFunc = M->getOrInsertFunction(
-            "printf", FunctionType::get(Type::getInt32Ty(Ctx),
-                      {Type::getInt8PtrTy(Ctx)}, true));
+        // 1. Préparer printf correctement
+        FunctionType *PrintfTy = FunctionType::get(
+            Type::getInt32Ty(Ctx),
+            {Type::getInt8PtrTy(Ctx)},
+            true /* variadic */);
+        
+        FunctionCallee PrintfFunc = M->getOrInsertFunction("printf", PrintfTy);
 
         // 2. Créer des variables globales opaques
         GlobalVariable *OpaqueCond = new GlobalVariable(
@@ -47,13 +50,14 @@ struct DeadBlockInsertionPass : public PassInfoMixin<DeadBlockInsertionPass> {
         // 4. Construction du bloc mort
         IRBuilder<> DeadBuilder(DeadBB);
         
-        // a) Effet de bord impossible à supprimer
+        // a) Effet de bord persistant
         Value *FormatStr = DeadBuilder.CreateGlobalStringPtr("DEAD_BLOCK_ACTIVATED\n");
         CallInst *PrintCall = DeadBuilder.CreateCall(PrintfFunc, {FormatStr});
-        PrintCall->setCannotBeOmitted(true);
+        PrintCall->setDoesNotThrow();
+        PrintCall->addFnAttr(Attribute::NoUnwind);
 
         // b) Opération mémoire opaque
-        Value *CounterVal = DeadBuilder.CreateLoad(Type::getInt32Ty(Ctx), OpaqueCounter);
+        LoadInst *CounterVal = DeadBuilder.CreateLoad(Type::getInt32Ty(Ctx), OpaqueCounter);
         Value *NewCounter = DeadBuilder.CreateAdd(CounterVal, DeadBuilder.getInt32(1));
         DeadBuilder.CreateStore(NewCounter, OpaqueCounter);
 
@@ -62,17 +66,13 @@ struct DeadBlockInsertionPass : public PassInfoMixin<DeadBlockInsertionPass> {
         DeadBuilder.CreateBr(LoopBB);
 
         IRBuilder<> LoopBuilder(LoopBB);
-        Value *ShouldExit = LoopBuilder.CreateLoad(Type::getInt1Ty(Ctx), OpaqueCond);
-        LoopBuilder.CreateCondBr(ShouldExit, RealBB, LoopBB);
+        LoadInst *CondLoad = LoopBuilder.CreateLoad(Type::getInt1Ty(Ctx), OpaqueCond);
+        LoopBuilder.CreateCondBr(CondLoad, RealBB, LoopBB);
 
         // 5. Modifier l'entrée originale
         IRBuilder<> EntryBuilder(&EntryBB->front());
-        EntryBuilder.CreateStore(EntryBuilder.getInt1(false), OpaqueCond);
+        EntryBuilder.CreateStore(ConstantInt::getFalse(Ctx), OpaqueCond);
         EntryBuilder.CreateBr(DeadBB);
-
-        // 6. Empêcher la suppression des éléments
-        OpaqueCond->setUnnamedAddr(GlobalValue::UnnamedAddr::None);
-        OpaqueCounter->setUnnamedAddr(GlobalValue::UnnamedAddr::None);
 
         return PreservedAnalyses::none();
     }
