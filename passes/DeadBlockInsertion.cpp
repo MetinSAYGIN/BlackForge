c7e510935bc93d5a9605dc930dfb5155fe617ce8
SAYGIN Metin <56536431+MetinSAYGIN@users.noreply.github.com>
2025-04-30 17:13:13 +0200

    Update DeadBlockInsertion.cpp

--- passes/DeadBlockInsertion.cpp
+++ passes/DeadBlockInsertion.cpp
@@ -8,14 +8,15 @@
 #include "llvm/IR/Constants.h"
 #include "llvm/IR/GlobalVariable.h"
 #include "llvm/IR/Module.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
 
 using namespace llvm;
 
 namespace {
 struct DeadBlockInsertionPass : public PassInfoMixin<DeadBlockInsertionPass> {
     PreservedAnalyses run(Function &F, FunctionAnalysisManager &) {
-        // Ne s'applique que sur les fonctions avec au moins un bloc
-        if (F.empty() || F.getReturnType()->isVoidTy() == false)
+        // Ne s'applique qu'aux fonctions non vides
+        if (F.empty())
             return PreservedAnalyses::all();
 
         errs() << "[+] Insertion de dead block dans : " << F.getName() << "\n";
@@ -23,36 +24,56 @@ struct DeadBlockInsertionPass : public PassInfoMixin<DeadBlockInsertionPass> {
         LLVMContext &Ctx = F.getContext();
         Module *M = F.getParent();
 
-        // Crée une chaîne constante globale
-        Constant *NeverReachedStr = ConstantDataArray::getString(Ctx, "Never reached\n", true);
-        GlobalVariable *GV = new GlobalVariable(
-            *M, NeverReachedStr->getType(), true, GlobalValue::PrivateLinkage,
-            NeverReachedStr, ".str.dead");
-
-        // Crée un pointeur vers la chaîne
-        Constant *Zero = ConstantInt::get(Type::getInt32Ty(Ctx), 0);
-        Constant *Indices[] = {Zero, Zero};
-        Constant *PrintfArg = ConstantExpr::getGetElementPtr(NeverReachedStr->getType(), GV, Indices);
-
-        // Déclare printf s'il n'existe pas
+        // Préparation de printf
         FunctionCallee PrintfFunc = M->getOrInsertFunction(
             "printf", FunctionType::get(IntegerType::getInt32Ty(Ctx),
-                                        PointerType::get(Type::getInt8Ty(Ctx), 0), true));
+                      PointerType::get(Type::getInt8Ty(Ctx), 0),
+                      true);
 
-        // Crée le bloc mort
+        // Création d'une chaîne globale opaque
+        GlobalVariable *GV = new GlobalVariable(
+            *M, ArrayType::get(Type::getInt8Ty(Ctx), 14),
+            true, GlobalValue::PrivateLinkage,
+            ConstantDataArray::getString(Ctx, "Never reached\n", true),
+            ".str.dead");
+
+        // Création du bloc mort
         BasicBlock *DeadBlock = BasicBlock::Create(Ctx, "deadblock", &F);
         IRBuilder<> Builder(DeadBlock);
-        Builder.CreateCall(PrintfFunc, {PrintfArg});
+        
+        // Construction d'un pointeur opaque vers la chaîne
+        Value *Zero = ConstantInt::get(Type::getInt32Ty(Ctx), 0);
+        Value *GEP = Builder.CreateInBoundsGEP(GV->getValueType(), GV, 
+                                              {Zero, Zero}, "dead.str.ptr");
+        
+        // Appel à printf avec des arguments opaques
+        Builder.CreateCall(PrintfFunc, {GEP});
         Builder.CreateRetVoid();
 
-        // C'est un dead block non connecté, donc ne sera jamais exécuté
+        // Insertion d'une branche conditionnelle toujours fausse mais difficile à analyser
+        BasicBlock &EntryBB = F.getEntryBlock();
+        Instruction *FirstInst = &EntryBB.front();
+        
+        IRBuilder<> EntryBuilder(FirstInst);
+        Value *GlobalAddr = EntryBuilder.CreatePtrToInt(GV, Type::getInt64Ty(Ctx));
+        Value *Mask = EntryBuilder.getInt64(0xFFFFFFFF);
+        Value *Cond = EntryBuilder.CreateICmpEQ(
+            EntryBuilder.CreateAnd(GlobalAddr, Mask),
+            EntryBuilder.getInt64(0));
+        
+        // Création d'un bloc intermédiaire qui mène au bloc mort
+        BasicBlock *DummyBB = BasicBlock::Create(Ctx, "dummy", &F);
+        BranchInst::Create(DeadBlock, DummyBB);
+        
+        // Remplacement de la première instruction par un branchement conditionnel
+        BranchInst::Create(DummyBB, &EntryBB, Cond, FirstInst);
+        FirstInst->eraseFromParent();
 
         return PreservedAnalyses::none();
     }
 };
 } // namespace
 
-// Point d'entrée pour LLVM
 extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo llvmGetPassPluginInfo() {
     return {
         LLVM_PLUGIN_API_VERSION,
@@ -67,6 +88,12 @@ extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo llvmGetPassPluginIn
                         return true;
                     }
                     return false;
+                });
+            
+            // S'enregistre aussi pour s'exécuter après les optimisations
+            PB.registerPipelineStartEPCallback(
+                [](ModulePassManager &MPM, OptimizationLevel) {
+                    MPM.addPass(createModuleToFunctionPassAdaptor(DeadBlockInsertionPass()));
                 });
         }
     };
