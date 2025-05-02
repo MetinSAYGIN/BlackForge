4b09e3461a8fecd970be20eebde107651ae41521
SAYGIN Metin <56536431+MetinSAYGIN@users.noreply.github.com>
2025-05-02 13:31:27 +0200

    Update AddUseless.cpp

--- passes/AddUseless.cpp
+++ passes/AddUseless.cpp
@@ -1,20 +1,16 @@
 #include <random>
-#include "llvm/Pass.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/LegacyPassManager.h"
-#include "llvm/Transforms/IPO/PassManagerBuilder.h"
 
 using namespace llvm;
 
 namespace {
-    struct AddUseless : public FunctionPass {
-        static char ID;
-        
-        AddUseless() : FunctionPass(ID) {}
-        
-        bool runOnFunction(Function &F) override {
+    struct AddUseless : public PassInfoMixin<AddUseless> {
+        PreservedAnalyses run(Function &F, FunctionAnalysisManager &) {
             // Générateur de nombres aléatoires
             std::random_device rd;
             std::default_random_engine generator(rd());
@@ -22,33 +18,39 @@ namespace {
             
             bool modified = false;
             
-            // Itérer sur tous les blocs de base (BasicBlocks)
+            // Itérer sur tous les blocs de base
             for (auto &BB : F) {
-                // Utiliser un IRBuilder pour insérer de nouvelles instructions
                 IRBuilder<> builder(&BB);
                 
                 // Ajouter des instructions inutiles avec une probabilité de 50%
                 if (distribution(generator) == 0) {
-                    // Création d'une instruction inutile (ajout d'une addition inutile)
+                    // Création d'une instruction inutile
                     builder.CreateAdd(builder.getInt32(0), builder.getInt32(0));
                     modified = true;
                 }
             }
             
-            return modified; // Retourne vrai si la fonction a été modifiée
+            return modified ? PreservedAnalyses::none() : PreservedAnalyses::all();
         }
     };
 }
 
-char AddUseless::ID = 0;
-
-// Enregistrement de la passe pour qu'elle soit reconnue par opt
-static RegisterPass<AddUseless> X("AddUseless", "Add useless instructions for obfuscation");
-
-// Intégration avec le système de passes standard
-static RegisterStandardPasses Y(
-    PassManagerBuilder::EP_EarlyAsPossible,
-    [](const PassManagerBuilder &Builder, legacy::PassManagerBase &PM) {
-        PM.add(new AddUseless());
-    }
-);
+// Registering the pass for LLVM 19.x
+extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK llvmGetPassPluginInfo() {
+    return {
+        .APIVersion = 0,
+        .PluginName = "AddUseless",
+        .PluginVersion = "v0.1",
+        .RegisterPassBuilderCallbacks = [](PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+                [](StringRef Name, FunctionPassManager &FPM,
+                   ArrayRef<PassBuilder::PipelineElement>) {
+                    if (Name == "AddUseless") {
+                        FPM.addPass(AddUseless());
+                        return true;
+                    }
+                    return false;
+                });
+        }
+    };
+}
