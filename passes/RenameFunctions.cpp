fc011e8f8266a14a185ce2642bff20b98aee3513
SAYGIN Metin <56536431+MetinSAYGIN@users.noreply.github.com>
2025-05-11 01:11:38 +0200

    s
    s

---
+++ passes/RenameFunctions.cpp
@@ -0,0 +1,46 @@
+#include "llvm/IR/PassManager.h"
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+namespace {
+struct RenameFunctionsPass : public PassInfoMixin<RenameFunctionsPass> {
+    PreservedAnalyses run(Module &M, ModuleAnalysisManager &) {
+        for (Function &F : M) {
+            if (F.isDeclaration()) continue;       // Ignore les fonctions externes
+            if (F.getName() == "main") continue;   // Ne pas renommer main
+
+            std::string oldName = F.getName().str();
+            std::string newName = "obf_" + oldName;
+
+            F.setName(newName);
+            errs() << "Renommé : " << oldName << " → " << newName << "\n";
+        }
+
+        return PreservedAnalyses::none(); // On modifie le module, donc rien n’est conservé
+    }
+};
+} // namespace
+
+
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo llvmGetPassPluginInfo() {
+    return {
+        LLVM_PLUGIN_API_VERSION,
+        "RenameFunctions",
+        LLVM_VERSION_STRING,
+        [](PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+                [](StringRef Name, ModulePassManager &MPM, ArrayRef<PassBuilder::PipelineElement>) {
+                    if (Name == "RenameFunctions") {
+                        MPM.addPass(RenameFunctionsPass());
+                        return true;
+                    }
+                    return false;
+                });
+        }
+    };
+}
+
