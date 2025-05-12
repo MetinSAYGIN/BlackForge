0a6b023e8e5091a221501a0d5e5a197531d9810b
SAYGIN Metin <56536431+MetinSAYGIN@users.noreply.github.com>
2025-05-12 19:49:14 +0200

    Update BlackForge.py

--- BlackForge.py
+++ BlackForge.py
@@ -155,6 +155,7 @@ else:
     # Obfuscation
     print("\n[+] Application de la passe d'obfuscation...")
     cmd = f"opt -load-pass-plugin {pass_so} -passes='default<O1>,{pass_name}' -S {clair_ll} -o {obf_ll}"
+    print(cmd)
     run_with_metrics(cmd)
     
     # Compilation finale
