bb069359f464dc55b71ec4944d0024ab41c30a6b
SAYGIN Metin <56536431+MetinSAYGIN@users.noreply.github.com>
2025-05-19 17:01:42 +0200

    Create main.c

---
+++ sources/clair/test_project/main.c
@@ -0,0 +1,12 @@
+// main.c
+#include <stdio.h>
+#include "fonctions.h"
+
+int main() {
+    dire_bonjour("Alice");
+    
+    int resultat = addition(3, 4);
+    printf("3 + 4 = %d\n", resultat);
+
+    return 0;
+}
