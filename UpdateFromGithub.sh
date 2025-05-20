25449fe0f8fb2664273cdddee21b4b3b23b3c18d
SAYGIN Metin <56536431+MetinSAYGIN@users.noreply.github.com>
2025-05-20 13:04:21 +0200

    Update UpdateFromGithub.sh

--- UpdateFromGithub.sh
+++ UpdateFromGithub.sh
@@ -4,7 +4,7 @@ git clone https://github.com/MetinSAYGIN/BlackForge.git
 
 cd BlackForge
 chmod -x BlackForge.py
-chmod -x /Compare.py
+chmod -x Compare.py
 git pull
 chmod +x BlackForge.py
 chmod +x Compare.py
