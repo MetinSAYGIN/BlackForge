1f3965ef8ab03f35afaeefb1babb6903e2ba8c69
SAYGIN Metin <56536431+MetinSAYGIN@users.noreply.github.com>
2025-05-28 01:36:58 +0200

    Update PrintfPrependPass.cpp

--- passes/PrintfPrependPass.cpp
+++ passes/PrintfPrependPass.cpp
@@ -5,43 +5,78 @@
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Type.h"
 
 using namespace llvm;
 
 namespace {
 struct PrintfPrependPass : public PassInfoMixin<PrintfPrependPass> {
     PreservedAnalyses run(Module &M, ModuleAnalysisManager &) {
-        // Récupère la fonction printf
-        Function *printfFunc = M.getFunction("printf");
+        Function *printfFunc = getOrCreatePrintf(M);
         if (!printfFunc) {
-            errs() << "La fonction printf n'a pas été trouvée\n";
+            errs() << "Failed to get printf function\n";
             return PreservedAnalyses::all();
         }
 
-        // Parcours toutes les fonctions du module
         for (Function &F : M) {
+            if (&F == printfFunc) continue;
+
             for (BasicBlock &BB : F) {
-                for (auto it = BB.begin(); it != BB.end(); ++it) {
-                    if (CallInst *callInst = dyn_cast<CallInst>(&*it)) {
-                        if (callInst->getCalledFunction() == printfFunc) {
-                            // Création d'un nouveau builder avant l'instruction actuelle
-                            IRBuilder<> Builder(&*it);
-                            
-                            // Ajoute un printf("########") avant chaque appel printf
-                            Value *formatStr = Builder.CreateGlobalStringPtr("########\n");
-                            Builder.CreateCall(printfFunc, {formatStr});
-                            
-                            errs() << "Ajout de printf(\"########\") avant un printf existant.\n";
-                        }
-                    }
+                processBasicBlock(BB, printfFunc);
+            }
+        }
+
+        return PreservedAnalyses::none();
+    }
+
+private:
+    Function* getOrCreatePrintf(Module &M) {
+        // Try different variants of printf names
+        if (Function *f = M.getFunction("printf"))
+            return f;
+        if (Function *f = M.getFunction("__printf"))
+            return f;
+
+        // Declare printf if not found
+        LLVMContext &Ctx = M.getContext();
+        FunctionType *printfType = FunctionType::get(
+            Type::getInt32Ty(Ctx),
+            {PointerType::getUnqual(Type::getInt8Ty(Ctx))},
+            true
+        );
+        return cast<Function>(M.getOrInsertFunction("printf", printfType).getCallee());
+    }
+
+    void processBasicBlock(BasicBlock &BB, Function *printfFunc) {
+        SmallVector<Instruction*, 16> ToInsertBefore;
+
+        for (Instruction &I : BB) {
+            if (CallInst *callInst = dyn_cast<CallInst>(&I)) {
+                Function *calledFunc = callInst->getCalledFunction();
+                if (calledFunc && isOutputFunction(calledFunc->getName())) {
+                    ToInsertBefore.push_back(&I);
                 }
             }
         }
 
-        return PreservedAnalyses::all();
+        for (Instruction *I : ToInsertBefore) {
+            IRBuilder<> Builder(I);
+            Value *formatStr = Builder.CreateGlobalStringPtr("########\n");
+            Builder.CreateCall(printfFunc, {formatStr});
+        }
+    }
+
+    bool isOutputFunction(StringRef name) {
+        // Match printf and puts variants using correct LLVM 19 API
+        return name == "printf" || 
+               name == "__printf" ||
+               name == "puts" ||
+               name == "_IO_puts" ||
+               name.starts_with("printf") ||
+               name.starts_with("puts");
     }
 };
-} // namespace
+}
 
 extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo llvmGetPassPluginInfo() {
     return {
